50b0b85287050c3c90633faca8bcaa1477dc7574
mcwachira <23430963+mcwachira@users.noreply.github.com>
2024-01-09 20:29:10 +0300

    chore:Created a custom image layout

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -18,7 +18,7 @@ const Gallery = async({topic}:Props) => {
 
     const photosWithBlur = await addBlurredDataUrls(images)
     return(
-        <section className='px-2 my-3 grid gap-2 grid-cols-gallery'>
+        <section className='px-2 my-3 grid gap-2 grid-cols-gallery auto-rows-[10px]'>
 
             <section className="px-2 my-3 grid gap-2 grid-cols-gallery">
 
